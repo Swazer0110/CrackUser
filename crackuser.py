40dc983adaf76a5a053e838bc68acfe5496877f4
Swazer0110 <75527943+Swazer0110@users.noreply.github.com>
2023-07-11 21:09:43 +0200

    v1.1

--- crackuser.py
+++ crackuser.py
@@ -4,16 +4,48 @@ import sys
 import argparse
 import itertools
 import chardet
+import re
 
-def apply_rule(path):
-    os.system("hashcat --stdout temp -r OneRuleToRuleThemAll.rule > {}".format(path))
+def banner():
+    print("""
+          _____             __   __  __           
+         / ___/______ _____/ /__/ / / /__ ___ ____
+        / /__/ __/ _ `/ __/  '_/ /_/ (_-</ -_) __/
+        \___/_/  \_,_/\__/_/\_\\____/___/\__/_/                                       
+        """)
+
+def apply_rule(rule,path):
+    print("!> Applying the rule [{}] to [{}]".format(rule,path))
+    os.system("hashcat --hook-threads=24 --stdout temp -r {} > {}".format(rule,path))
+
 
 def save_data(data,path):
     with open(path,"w")as file:
         file.write("\n".join(data))
 
+def remove_duplicates(path):
+    with open(path, 'r', encoding='utf-8', errors='replace') as f:
+        wordlist = f.read().splitlines()
+
+    unique_wordlist = list(set(wordlist))
+
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(unique_wordlist))
+
+
+def errors(args):
+    if args.strings:
+       data = args.strings.split(",")
+    else:
+        print("No strings specified")
+        exit()
+
+    if args.allowed and args.disallowed:
+        print("Incompatible flags: -allowed -disallowed")
+        exit()
 
 def generate_permutations(data, min_words, max_words):
+    print("!> Generating string combinations")
     permutations = []
     
     for r in range(min_words, max_words + 1):
@@ -22,89 +54,76 @@ def generate_permutations(data, min_words, max_words):
     
     return permutations
 
+def filter_word(word, regex, mode):
+    if mode == "allowed":
+        regex="^[{}]+$".format(regex)
+        if re.match(regex, word):
+            return word
+    elif mode == "disallowed":
+        if not re.search(f"[{regex}]", word):
+            return word
+    elif mode == "regex":
+        if re.match(regex, word):
+            return word
+    return None
 
-def allowed(filep, allowed_characters):
+def regex_filter(filep, regex, mode):
+    print("!> Applying {} regex to [{}]".format(mode, filep))
     filtered_wordlist = []
-    with open(filep, 'r') as file:
-        raw_data = file.read()
-        result = chardet.detect(raw_data)
-        encoding = result['encoding']
-        wordlist = raw_data.decode(encoding).splitlines()
-    for word in wordlist:
-        if re.match(f"^[{allowed_characters}]+$", word):
-            filtered_wordlist.append(word)
-    with open(filep, 'w') as file:
-        for word in filtered_wordlist:
-            file.write(word + '\n')
+    with open(filep, 'r', encoding='latin-1') as file:
+        wordlist = file.read().splitlines()
 
-
-def disallowed(filep, not_allowed_characters):
-    filtered_wordlist = []
-    with open(filep, 'r') as file:
-        raw_data = file.read()
-        result = chardet.detect(raw_data)
-        encoding = result['encoding']
-        wordlist = raw_data.decode(encoding).splitlines()
     for word in wordlist:
-        if not re.search(f"[{not_allowed_characters}]", word):
-            filtered_wordlist.append(word)
-    with open(filep, 'w') as file:
-        for word in filtered_wordlist:
-            file.write(word + '\n')
+        result = filter_word(word, regex, mode)
+        if result is not None:
+            filtered_wordlist.append(result)
+
+    with open(filep, 'w', encoding='latin-1') as file:
+        file.write('\n'.join(filtered_wordlist))
+
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Create wordlist with combinations of given data')
-    parser.add_argument('-s', '--strings', type=str, required=False, help='Strings to combine (separated by commas) Ex: -s word1,word2,word3')
-    parser.add_argument('-r', '--rule', type=str, required=False, help='Hashcat rule')
-    parser.add_argument('-o', '--output', type=str, required=False, help='Output file path')
-    parser.add_argument('-max', '--max-combinations', type=str, required=False, help='Max number of combined word together')
-    parser.add_argument('-min', '--min-combinations', type=str, required=False, help='Minimun number of combined word together')
+    banner()
+    parser = argparse.ArgumentParser(description="Create wordlist with combinations of given data")
+    parser.add_argument('-s', '--strings', type=str, required=True, help='Strings to combine (separated by commas) Ex: -s word1,word2,word3')
+    parser.add_argument('-r', '--rule', type=str, required=False, default="OneRuleToRuleThemAll.rule", help='Hashcat rule')
+    parser.add_argument('-o', '--output', type=str, required=False, default="Output/wordlist.txt", help='Output file path')
+    parser.add_argument('-max', '--max-combinations', type=int, required=False, default=3, help='Max number of combined word together')
+    parser.add_argument('-min', '--min-combinations', type=int, required=False, default=1, help='Minimum number of combined word together')
     parser.add_argument('-a', '--allowed', type=str, required=False, help='Allowed characters in the wordlist. Ex: -a abcdefghijklmnopqrstxyz-_,')
     parser.add_argument('-d', '--disallowed', type=str, required=False, help='Disallowed characters in the wordlist. Ex: -d *?_:.;,')
-
-
+    parser.add_argument('-re', '--regex', type=str, required=False, help='Custom regex to filter wordlist. Ex: -re "^[{abcdefghijklmnopqrstxyz}]+$"')
 
     args = parser.parse_args()
+    errors(args)
 
-    if args.strings:
-       data = args.strings.split(",")
-    else:
-        print("No strings specified")
-        exit()
-
-
-    if args.max_combinations:
-        max_comb = args.max_combinations
-    else:
-        max_comb = len(data)
-
-    if args.min_combinations:
-        min_comb = args.min_combinations
-    else:
-        min_comb = 0
-
-
-    if args.output:
-        path=args.output
-    else:
-        path="wordlist.txt"
-
-    
-    if args.allowed and args.disallowed:
-        print("Incompatible flags: -allowed -disallowed")
-        exit()
-    
-    
-
-    combinations = generate_permutations(data,min_comb,max_comb)[1:]
-    print(combinations)
-    save_data(combinations, "temp")
-    apply_rule(path)
-    os.system("rm temp")
+    data = args.strings.split(",")
+    rule=args.rule
+    path=args.output
+    min_comb = args.min_combinations
+    max_comb = args.max_combinations
+    mode=None
+    regex=None
     if args.allowed:
-        allowed(path,args.allowed)
+        mode="allowed"
+        regex=args.allowed
     if args.disallowed:
-        disallowed(path,args.disallowed)
+        mode="disallowed"
+        regex=args.disallowed
+    if args.regex:
+        mode="regex"
+        regex=args.regex
+    
+    combinations = generate_permutations(data,min_comb,max_comb)
+    save_data(combinations, "temp")
+    apply_rule(rule,path)
+        
+    if regex: regex_filter(path, regex, mode)
+
+    remove_duplicates(path)
+    
+    os.system("rm temp")
+
 if __name__ == "__main__":
     main()
